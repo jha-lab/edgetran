1459fb5b18c1789b1d566883c7c2631cba035ed9
shikhartuli <shikhartuli98@gmail.com>
2022-03-08 15:33:16 -0500

    Added learning rate

--- grow_and_prune/pretrain_model.py
+++ grow_and_prune/pretrain_model.py
@@ -29,7 +29,7 @@ import logging
 PREFIX_CHECKPOINT_DIR = "checkpoint"
 
 
-def _get_training_args(seed, max_steps, per_gpu_batch_size, output_dir, local_rank):
+def _get_training_args(seed, max_steps, learning_rate, per_gpu_batch_size, output_dir, local_rank):
     a = "--seed {} \
     --do_train \
     --max_seq_length 512 \
@@ -38,14 +38,14 @@ def _get_training_args(seed, max_steps, per_gpu_batch_size, output_dir, local_ra
     --max_steps {} \
     --adam_epsilon 1e-6 \
     --adam_beta2 0.98 \
-    --learning_rate 1e-5 \
+    --learning_rate {} \
     --weight_decay 0.01 \
     --save_total_limit 2 \
     --warmup_steps 1000 \
     --lr_scheduler_type linear \
     --output_dir {} \
     --local_rank {} \
-        ".format(seed, per_gpu_batch_size, max_steps, output_dir, local_rank)
+        ".format(seed, per_gpu_batch_size, max_steps, learning_rate, output_dir, local_rank)
     return shlex.split(a)
 
 
@@ -67,7 +67,7 @@ def main(args):
 	max_steps = curr_steps + args.steps
 
 	seed = 0
-	training_args = _get_training_args(seed, max_steps, 16, args.output_dir, args.local_rank)
+	training_args = _get_training_args(seed, max_steps, args.learning_rate, 16, args.output_dir, args.local_rank)
 
 	# Get model dictionary from output directory
 	model_dict = json.load(open(os.path.join(args.output_dir, 'model_dict.json'), 'r'))
@@ -97,6 +97,10 @@ if __name__ == '__main__':
 		metavar='',
 		type=int,
 		help='number of steps to pre-train beyond latest checkpoint')
+	parser.add_argument('--learning_rate',
+		metavar='',
+		type=int,
+		help='learning rate to set for training')
 	parser.add_argument('--local_rank',
 		metavar='',
 		type=int,
